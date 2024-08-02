5603ffbc801f77bd90bbf3ea6248c52acff51ed6
bappi <bappi.chandra@amiprobashi.com>
2024-08-02 23:06:38 +0600

    complete header section

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuBtn = document.getElementById('menuBtn');
+const sideNav = document.getElementById('sideNav');
+
+sideNav.style.right = '-250px';
+menuBtn.onclick = function() {
+    if (sideNav.style.right == '-250px') {
+        sideNav.style.right = '0'
+    }
+    else{
+        sideNav.style.right = '-250px';
+    }
+}
